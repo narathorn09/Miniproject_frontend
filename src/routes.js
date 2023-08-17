008ee91d9bd8008865cc55bc5e2fd2dfd40964f1
Narathorn Noophum <6310210181@psu.ac.th>
2023-08-17 14:07:27 +0700

    fix routes for usertype

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,34 @@
 import HomePage from './pages/Home.vue';
+import CreateBoardGame from './pages/CreateBoardGame.vue';
+import ListBoardGame from './pages/ListBoardGame.vue';
+import EditBoardGame from './pages/EditBoardGame.vue';
+import ListUser from './pages/ListUser.vue';
+import CreateUser from './pages/CreateUser.vue';
+import EditUser from './pages/EditUser.vue';
 
-const routes = [
-  { path: '/', component: HomePage, meta: { title: 'Home - BoardGame' } },
-  { path: '/adminmenu', component: null, meta: { title: 'Admin Menu - BoardGame' } },
-];
+const auth = JSON.parse(localStorage.getItem('auth'));
+
+let routes = []
+
+if(auth?.userType === '1'){
+  routes = [
+    { path: '/', component: HomePage, meta: { title: 'Home - BoardGame' } },
+    { path: '/manage-boardgame', component: ListBoardGame, meta: { title: 'Admin Menu - BoardGame' } },
+    { path: '/create-boardgame', component: CreateBoardGame, meta: { title: 'Admin Menu - BoardGame' } },
+    { path: '/edit-boardgame/:gameId', component: EditBoardGame, meta: { title: 'Admin Menu - BoardGame' } },
+    { path: '/manage-user', component: ListUser, meta: { title: 'Admin Menu - BoardGame' } },
+    { path: '/create-user', component: CreateUser, meta: { title: 'Admin Menu - BoardGame' } },
+    { path: '/edit-user/:userId', component: EditUser, meta: { title: 'Admin Menu - BoardGame' } },
+  ];
+}else if(auth?.userType === '0'){
+   routes = [
+    { path: '/', component: HomePage, meta: { title: 'Home - BoardGame' } },
+  ];
+}else{
+  routes = [
+    { path: '/', component: HomePage, meta: { title: 'Home - BoardGame' } },
+  ];
+}
+
 
 export default routes;
